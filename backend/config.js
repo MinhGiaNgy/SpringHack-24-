2975718b3b1ed212aaa9a84caf4efdd2bca9f19e
Gia Minh Nguyen <95914730+MinhGiaNgy@users.noreply.github.com>
2024-07-21 00:04:54 +0700

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5000;
+
+export const mongoDBURL = 
+    'mongodb+srv://admin:McbuZKypGGpcXDJA@tutorial.b6avwek.mongodb.net/cards-collection?retryWrites=true&w=majority&appName=Tutorial'
